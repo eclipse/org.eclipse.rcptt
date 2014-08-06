4937486339026ba4e0cde638c9b9905e33ea237a
Gulevich Vasili <vasili.gulevich@xored.com>
2014-08-06 16:52:49 +0700

    QS-3262 Time verification now keeps its state in global variables.
    A workaround to calculate execution time properly after AUT restart.

--- runtime/verifications/org.eclipse.rcptt.verifications.time.impl/src/org/eclipse/rcptt/verifications/time/impl/TimeVerificationProcessor.java
+++ runtime/verifications/org.eclipse.rcptt.verifications.time.impl/src/org/eclipse/rcptt/verifications/time/impl/TimeVerificationProcessor.java
@@ -17,27 +17,52 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.rcptt.core.VerificationProcessor;
 import org.eclipse.rcptt.core.scenario.Verification;
+import org.eclipse.rcptt.ecl.core.CoreFactory;
+import org.eclipse.rcptt.ecl.core.Val;
+import org.eclipse.rcptt.ecl.internal.core.AbstractSession;
+import org.eclipse.rcptt.ecl.internal.core.DeclarationContainer;
+import org.eclipse.rcptt.ecl.runtime.BoxedValues;
 import org.eclipse.rcptt.ecl.runtime.IProcess;
 import org.eclipse.rcptt.util.MathUtils;
 import org.eclipse.rcptt.verifications.time.TimeVerification;
 
 public class TimeVerificationProcessor extends VerificationProcessor {
 
+	private static final String START_KEY = TimeVerificationProcessor.class.getName() + "_start";
+	private static final String RUN_KEY = TimeVerificationProcessor.class.getName() + "_run";
+
 	@Override
 	public Object start(Verification verification, IProcess process) throws CoreException {
-		return new Date();
+		putTime(process, START_KEY);
+		return null;
+	}
+
+	private static void putTime(IProcess process, String key) throws CoreException {
+		Val val = CoreFactory.eINSTANCE.createVal();
+		val.setName(key);
+		val.setValue(BoxedValues.box(new Date().getTime()));
+		getGlobals(process).declare(val.getName(), val);
+	}
+
+	private static Date getDate(IProcess process, final String key) {
+		long startTime = (Long) BoxedValues.unbox((((Val) getGlobals(process)
+				.lookup(key)).getValue()));
+		return new Date(startTime);
 	}
 
 	@Override
 	public Object run(Verification verification, Object data, IProcess process) throws CoreException {
-		final TimeVerification timeVerification = (TimeVerification) verification;
-		return timeVerification.isIncludeContexts() ? data : new Date();
+		putTime(process, RUN_KEY);
+		return null;
 	}
 
 	@Override
 	public void finish(Verification verification, Object data, IProcess process) throws CoreException {
 		final TimeVerification timeVerification = (TimeVerification) verification;
-		final Date start = (Date) data;
+
+		final String key = timeVerification.isIncludeContexts() ? START_KEY : RUN_KEY;
+
+		final Date start = getDate(process, key);
 
 		double diff = (new Date().getTime() - start.getTime()) / 1000.0;
 		double goal = timeVerification.getMinutes() * 60 +
@@ -54,6 +79,12 @@ public class TimeVerificationProcessor extends VerificationProcessor {
 							String.format(
 									"Verification '%s' failed. Expected test case execution time is %.2f second(s), but it was %.2f second(s).",
 									verification.getName(), goal, diff)));
+	}
+
+
+	private static DeclarationContainer getGlobals(IProcess process) {
+		return ((AbstractSession) process.getSession())
+				.getRoot().getGlobals();
 	}
 
 }
