84463e8110c0197a8fca221bb7875f4191c51dc8
Ivan Inozemtsev <ivan.inozemtsev@xored.com>
2014-08-13 16:29:39 +0700

    copy necessary files to download area

--- releng/publish.sh
+++ releng/publish.sh
@@ -10,6 +10,7 @@ echo "productQualifier: $productQualifier"
 
 downloadsHome=/home/data/httpd/download.eclipse.org/rcptt
 type=nightly
+typeAbbr=N
 
 # where all downloads of this branch reside
 streamDestination=$downloadsHome/$type/$productVersion
@@ -17,4 +18,34 @@ streamDestination=$downloadsHome/$type/$productVersion
 buildDestination=$streamDestination/$productVersion.$productQualifier
 echo "stream destination: $streamDestination "
 echo "build destination: $buildDestination "
+
+
+# copying products
+
+productsDir=$WORKSPACE/repository/full/target/products
+productsDestination=$buildDestination/ide
+
+mkdir -p $productsDestination
+cp $productsDir/org.eclipse.rcptt.platform.product-linux.gtk.x86.zip $productsDestination/rcptt.ide-1.5.1-$typeAbbr$productQualifier-linux.gtk.x86.zip
+cp $productsDir/org.eclipse.rcptt.platform.product-linux.gtk.x86_64.zip $productsDestination/rcptt.ide-1.5.1-$typeAbbr$productQualifier-linux.gtk.x86_64.zip
+cp $productsDir/org.eclipse.rcptt.platform.product-macosx.cocoa.x86_64.zip $productsDestination/rcptt.ide-1.5.1-$typeAbbr$productQualifier-macosx.cocoa.x86_64.zip
+cp $productsDir/org.eclipse.rcptt.platform.product-win32.win32.x86.zip $productsDestination/rcptt.ide-1.5.1-$typeAbbr$productQualifier-win32.win32.x86.zip
+cp $productsDir/org.eclipse.rcptt.platform.product-win32.win32.x86_64.zip $productsDestination/rcptt.ide-1.5.1-$typeAbbr$productQualifier-win32.win32.x86_64.zip
+
+# copy full repository
+cp -r $WORKSPACE/repository/full/target/repository $buildDestination
+
+
+# copy runtimes
+cp -r $WORKSPACE/runtime/updates/org.eclipse.rcptt.updates.runtime/q7 $buildDestination/runtime3x
+cp -r $WORKSPACE/runtime/updates/org.eclipse.rcptt.updates.runtime.e4x/q7 $buildDestination/runtime4x
+
+
+# echo results
+
+find $buildDestination
+
 # discard old builds
+
+
+#  symlink latest
