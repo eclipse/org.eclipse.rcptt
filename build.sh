4bb7d0fe19d959e2b7af64ec170bd1a370eb2eed
Ivan Inozemtsev <ivan.inozemtsev@xored.com>
2014-08-25 19:09:01 +0700

    scripts for local build

---
+++ build.sh
@@ -0,0 +1,27 @@
+export MAVEN_OPTS="-Xmx512m -XX:MaxPermSize=256m"
+
+mvn clean verify -f releng/core/pom.xml \
+    -DheliosRepository=http://updates.xored.com/helios \
+    -DindigoRepository=http://updates.xored.com/indigo \
+    -DkeplerRepository=http://updates.xored.com/kepler
+
+mvn clean verify -f releng/runtime/pom.xml -P runtime3x \
+    -DheliosRepository=http://updates.xored.com/helios \
+    -DindigoRepository=http://updates.xored.com/indigo \
+    -DkeplerRepository=http://updates.xored.com/kepler \
+    -DcoreRepository=file:$PWD/repository/core/target/repository
+
+mvn clean verify -f releng/runtime/pom.xml -P runtime4x \
+    -DheliosRepository=http://updates.xored.com/helios \
+    -DindigoRepository=http://updates.xored.com/indigo \
+    -DkeplerRepository=http://updates.xored.com/kepler \
+    -DcoreRepository=file:$PWD/repository/core/target/repository
+
+mvn clean verify -f releng/ide/pom.xml \
+    -DheliosRepository=http://updates.xored.com/helios \
+    -DindigoRepository=http://updates.xored.com/indigo \
+    -DkeplerRepository=http://updates.xored.com/kepler \
+    -DcoreRepository=file:$PWD/repository/core/target/repository \
+    -De3xRepository=file:$PWD/repository/e3x/target/repository \
+    -De4xRepository=file:$PWD/repository/e4x/target/repository"
+
