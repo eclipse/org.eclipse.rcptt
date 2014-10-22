11cffac3a9d00b25fdd2187794d3798ca80a9af9
Gulevich Vasili <vasili.gulevich@xored.com>
2014-10-22 22:34:59 +0700

    Fixed build script to fail on errors

--- build.cmd
+++ build.cmd
@@ -3,22 +3,27 @@ set PWD=%CD:\=/%
 
 set TARGETS=clean verify
 
+call mvn clean verify -f clean-pom.xml ||| exit 102
+
 call mvn %TARGETS% -f releng/core/pom.xml ^
     -DheliosRepository=http://updates.xored.com/helios ^
     -DindigoRepository=http://updates.xored.com/indigo ^
-    -DkeplerRepository=http://updates.xored.com/kepler
+    -DkeplerRepository=http://updates.xored.com/kepler ^
+    || exit 103
 
 call mvn %TARGETS% -f releng/runtime/pom.xml -P runtime3x ^
     -DheliosRepository=http://updates.xored.com/helios ^
     -DindigoRepository=http://updates.xored.com/indigo ^
     -DkeplerRepository=http://updates.xored.com/kepler ^
-    -DcoreRepository=file:%PWD%/repository/core/target/repository
+    -DcoreRepository=file:%PWD%/repository/core/target/repository ^
+    || exit 104
 
 call mvn %TARGETS% -f releng/runtime/pom.xml -P runtime4x ^
     -DheliosRepository=http://updates.xored.com/helios ^
     -DindigoRepository=http://updates.xored.com/indigo ^
     -DkeplerRepository=http://updates.xored.com/kepler ^
-    -DcoreRepository=file:%PWD%/repository/core/target/repository
+    -DcoreRepository=file:%PWD%/repository/core/target/repository ^
+    || exit 105
 
 call mvn %TARGETS% -f releng/ide/pom.xml ^
     -DheliosRepository=http://updates.xored.com/helios ^
@@ -26,5 +31,6 @@ call mvn %TARGETS% -f releng/ide/pom.xml ^
     -DkeplerRepository=http://updates.xored.com/kepler ^
     -DcoreRepository=file:%PWD%/repository/core/target/repository ^
     -De3xRepository=file:%PWD%/repository/e3x/target/repository ^
-    -De4xRepository=file:%PWD%/repository/e4x/target/repository
+    -De4xRepository=file:%PWD%/repository/e4x/target/repository ^
+    || exit 106
 
