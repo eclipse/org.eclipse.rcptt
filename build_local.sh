8b325f167b92e0f326e84b99d0bdc3b549bab28a
Andrey Sobolev <haiodo@gmail.com>
2015-10-15 18:10:44 +0600

    Build update

--- build_local.sh
+++ build_local.sh
@@ -4,40 +4,45 @@ export MAVEN_OPTS="-Xms512m -Xmx756m -XX:MaxPermSize=256m"
 export MAVEN_EXTRA="-Dmaven.repo.local=./m2"
 OPTIONS="-Dtycho.localArtifacts=ignore $MAVEN_EXTRA $@"
 
-read -p "\nBuild Core(y/n)? " -n 1 -r
+echo ""
+read -p "Build Core(y/n)? " -e -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-mvn clean install -f releng/core/pom.xml $OPTIONS || exit 101
+mvn clean install -f releng/core/pom.xml $OPTIONS
 fi
 
 export REPLY=""
 
-read -p "\nBuild Runtime 3x(y/n)? " -n 1 -r
+echo ""
+read -p "Build Runtime 3x(y/n)? " -e -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-mvn clean install -f releng/runtime/pom.xml -P runtime3x $OPTIONS || exit 102
+mvn clean install -f releng/runtime/pom.xml -P runtime3x $OPTIONS
 fi
 
 export REPLY=""
 
-read -p "\nBuild Runtime 4x(y/n)? " -n 1 -r
+echo ""
+read -p "Build Runtime 4x(y/n)? " -e -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-mvn clean install -f releng/runtime/pom.xml -P runtime4x $OPTIONS || exit 103
+mvn clean install -f releng/runtime/pom.xml -P runtime4x $OPTIONS
 fi
 
 export REPLY=""
 
-read -p "\nBuild IDE and modules(y/n)? " -n 1 -r
+echo ""
+read -p "Build IDE and modules(y/n)? " -e -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-mvn clean install -f releng/ide/pom.xml $OPTIONS || exit 104
+mvn clean install -f releng/ide/pom.xml $OPTIONS
 fi
 
 export REPLY=""
 
-read -p "\nBuild Product(y/n)? " -n 1 -r
+echo ""
+read -p "Build Product(y/n)? " -e -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-mvn clean install -f releng/rcptt/pom.xml $OPTIONS || exit 105
+mvn clean install -f releng/rcptt/pom.xml $OPTIONS
 fi
