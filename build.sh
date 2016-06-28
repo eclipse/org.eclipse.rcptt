4f4f601d1327a6e67b8f1accd6b335631310523a
Andrey Sobolev <haiodo@gmail.com>
2016-06-28 11:51:48 +0600

    * Fix copyrights

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+#*******************************************************************************
+# Copyright (c) 2009, 2016 Xored Software Inc and others.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#  
+# Contributors:
+# 	Xored Software Inc - initial API and implementation and/or initial documentation
+#*******************************************************************************
 export MAVEN_OPTS="-Xms512m -Xmx1024m -XX:MaxMetaspaceSize=256m -XX:MaxPermSize=256m"
 
 OPTIONS="-Dtycho.localArtifacts=ignore $@"
--- ecl/plugins/org.eclipse.rcptt.ecl.data/src/au/com/bytecode/opencsv/ResultSetHelperService.java
+++ ecl/plugins/org.eclipse.rcptt.ecl.data/src/au/com/bytecode/opencsv/ResultSetHelperService.java
@@ -1,6 +1,6 @@
 package au.com.bytecode.opencsv;
 /**
- Copyright 2005 Bytecode Pty Ltd.
+ Copyright 2005, 2014 Bytecode Pty Ltd.
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
