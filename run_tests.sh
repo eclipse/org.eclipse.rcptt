bdd4682f4c0804594dee6d449c8af501e85762b8
Alexander Dudaev <Aleksandr.Dudayev@xored.com>
2019-09-17 11:23:19 +0700

    Version update automation
    Signed-off-by: Alexander Dudaev <Aleksandr.Dudayev@xored.com>
    Change-Id: Ic8158747e3519539d479603bd6ffe14e60a6ac3e

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #*******************************************************************************
-# Copyright (c) 2009, 2016 Xored Software Inc and others.
+# Copyright (c) 2009, 2019 Xored Software Inc and others.
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License v1.0
 # which accompanies this distribution, and is available at
@@ -12,5 +12,5 @@
 . repository/full/target/publisher.properties
 mvn clean verify -B -f rcpttTests/pom.xml \
 -DrcpttPath=`readlink -f repository/full/target/products/org.eclipse.rcptt.platform.product-linux.gtk.x86_64.zip` \
--DexplicitRunner=`readlink -f runner/product/target/rcptt.runner-${productVersion}-SNAPSHOT.zip` || true
+-DexplicitRunner=`readlink -f runner/product/target/rcptt.runner-*.zip` || true
 test -f rcpttTests/target/results/tests.html
