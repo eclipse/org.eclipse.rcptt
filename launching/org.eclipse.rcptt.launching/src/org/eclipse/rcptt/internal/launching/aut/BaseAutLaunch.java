eclipse/org.eclipse.rcptt

commit 86e1122c956190ef23390e8695c3440bd9c12320

Restart Aut after several tests fail with closing modal dialogs Know if context is default.
Change-Id: Ic31c1b5a7ce9ef0ccdd714903af8236a375603fc
Signed-off-by: Galina Shvetsova <galina.shvetsova@xored.com>

launching/org.eclipse.rcptt.launching/src/org/eclipse/rcptt/internal/launching/aut/BaseAutLaunch.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2019 Xored Software Inc and others.
+ * Copyright (c) 2009, 2020 Xored Software Inc and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -295,9 +295,17 @@ public class BaseAutLaunch implements AutLaunch, IBaseAutLaunchRetarget {
 		return (T) result[0];
 	}
 
+	private IStatus createInternalAutFailStatus(IContext context) {
+		final IStatus status = new Status(IStatus.ERROR, CorePlugin.PLUGIN_ID,
+				context.isBuiltin() ? IProcess.INTERNAL_AUT_FAILURE : 0,
+				"Failed to apply context " + context.getName(), new FailToExecuteDefaultContext());
+		final IStatus statusWithAdvancedInfo = ExecAdvancedInfoUtil.askForAdvancedInfo(this, status);
+		return statusWithAdvancedInfo;
+	}
+
 	private IStatus createTimeoutStatus(final long timeout) {
 		final IStatus status = new Status(IStatus.ERROR, CorePlugin.PLUGIN_ID, IProcess.TIMEOUT_CODE,
-			"Execution has timed out after " + (timeout / 1000.) + " seconds", new TimeoutException());
+				"Execution has timed out after " + (timeout / 1000.) + " seconds", new TimeoutException());
 		final IStatus statusWithAdvancedInfo = ExecAdvancedInfoUtil.askForAdvancedInfo(this, status);
 		return statusWithAdvancedInfo instanceof ExecutionStatus ? statusWithAdvancedInfo : status;
 	}
@@ -343,8 +351,9 @@ public class BaseAutLaunch implements AutLaunch, IBaseAutLaunchRetarget {
 		ArrayList<Object> result = new ArrayList<Object>();
 		while (true) {
 			Object o = out.take(timeout);
-			if (o instanceof IStatus)
+			if (o instanceof IStatus) {
 				return result;
+			}
 			result.add(o);
 		}
 	}
@@ -604,12 +613,12 @@ public class BaseAutLaunch implements AutLaunch, IBaseAutLaunchRetarget {
 		ec.setData(context);
 		try {
 			final IStatus result = internalExecute(ec, TeslaLimits.getContextRunnableTimeout(), monitor, null);
+			IStatus status = createInternalAutFailStatus(contextElement);
 			if (!result.isOK()) {
-				throw new CoreException(ExecAdvancedInfoUtil.askForAdvancedInfo(this, result));
+				throw new CoreException(status);
 			}
 		} catch (InterruptedException e) {
-			throw new CoreException(ExecAdvancedInfoUtil.askForAdvancedInfo(this,
-					"Failed to apply context: " + contextElement.getElementName()));
+			throw new CoreException(createInternalAutFailStatus(contextElement));
 		}
 	}
 
@@ -755,8 +764,9 @@ public class BaseAutLaunch implements AutLaunch, IBaseAutLaunchRetarget {
 				if (!status.isOK()) {
 					throw new CoreException(status);
 				}
-				if (!it.hasNext())
+				if (!it.hasNext()) {
 					break;
+				}
 				if (debugger != null) {
 					debugger.beforeRestart();
 				}
@@ -845,8 +855,9 @@ public class BaseAutLaunch implements AutLaunch, IBaseAutLaunchRetarget {
 		} finally {
 			try {
 				while (stop > System.currentTimeMillis()) {
-					if (launch.isTerminated())
+					if (launch.isTerminated()) {
 						return;
+					}
 					Thread.sleep(1000);
 				}
 			} finally {

core/org.eclipse.rcptt.core/src/org/eclipse/rcptt/core/model/IContext.java
@@ -14,4 +14,5 @@ import org.eclipse.rcptt.core.ContextType;
 
 public interface IContext extends IQ7NamedElement {
 	ContextType getType() throws ModelException;
+	public boolean isBuiltin();
 }

core/org.eclipse.rcptt.core/src/org/eclipse/rcptt/internal/core/model/Q7Context.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2019 Xored Software Inc and others.
+ * Copyright (c) 2009, 2020 Xored Software Inc and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -56,5 +56,10 @@ public class Q7Context extends Q7NamedElement implements IContext {
 							"Could not create context working copy with unexisted resource"));
 		}
 		return super.getWorkingCopy(monitor);
+	}
+	
+	@Override
+	public boolean isBuiltin() {
+		return false;
 	}
 }

core/org.eclipse.rcptt.core/src/org/eclipse/rcptt/internal/core/model/Q7InternalContext.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2019 Xored Software Inc and others.
+ * Copyright (c) 2009, 2020 Xored Software Inc and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -105,6 +105,11 @@ public class Q7InternalContext extends Q7Context {
 	protected boolean buildStructure(OpenableElementInfo info,
 			IProgressMonitor pm, Map<IQ7Element, Object> newElements,
 			IResource underlyingResource) throws ModelException {
+		return true;
+	}
+	
+	@Override
+	public boolean isBuiltin() {
 		return true;
 	}
 }

ecl/plugins/org.eclipse.rcptt.ecl.core/src/org/eclipse/rcptt/ecl/runtime/IProcess.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2019 Xored Software Inc and others.
+ * Copyright (c) 2009, 2020 Xored Software Inc and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -17,6 +17,7 @@ import org.eclipse.core.runtime.IStatus;
 public interface IProcess {
 
 	public static final int TIMEOUT_CODE = 123130;
+	public static final int INTERNAL_AUT_FAILURE = 123131;
 
 	IPipe getInput();
 

launching/org.eclipse.rcptt.launching/src/org/eclipse/rcptt/internal/launching/PrepareExecutionWrapper.java
@@ -39,8 +39,10 @@ import org.eclipse.rcptt.ecl.core.Sequence;
 import org.eclipse.rcptt.ecl.core.util.ECLBinaryResourceImpl;
 import org.eclipse.rcptt.ecl.core.util.ScriptletFactory;
 import org.eclipse.rcptt.ecl.internal.core.ProcessStatusConverter;
+import org.eclipse.rcptt.ecl.runtime.IProcess;
 import org.eclipse.rcptt.internal.core.RcpttPlugin;
 import org.eclipse.rcptt.internal.launching.ecl.EclScenarioExecutable;
+import org.eclipse.rcptt.internal.launching.ecl.ExecAdvancedInfoUtil;
 import org.eclipse.rcptt.launching.AutLaunch;
 import org.eclipse.rcptt.launching.IExecutable;
 import org.eclipse.rcptt.launching.Q7LaunchUtils;
@@ -131,7 +133,10 @@ public class PrepareExecutionWrapper extends Executable {
 			resetParams();
 			resetVerifications();
 		} catch (CoreException e) {
-			return e.getStatus();
+			IStatus status = e.getStatus();
+			IStatus newStatus = new Status(IStatus.ERROR, status.getPlugin(), IProcess.INTERNAL_AUT_FAILURE,
+					status.getMessage(), status.getException());
+			return ExecAdvancedInfoUtil.askForAdvancedInfo(launch, newStatus);
 		} catch (Exception e) {
 			return Q7LaunchingPlugin.createStatus(e);
 		}

launching/org.eclipse.rcptt.launching/src/org/eclipse/rcptt/internal/launching/aut/FailToExecuteDefaultContext.java
@@ -1,0 +1,13 @@
+package org.eclipse.rcptt.internal.launching.aut;
+
+public class FailToExecuteDefaultContext extends Exception {
+	private static final long serialVersionUID = -6947749663598564602L;
+
+	public FailToExecuteDefaultContext() {
+	}
+
+	public FailToExecuteDefaultContext(String message) {
+		super(message);
+	}
+
+}

launching/org.eclipse.rcptt.launching/src/org/eclipse/rcptt/internal/launching/ecl/ExecAdvancedInfoUtil.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2019 Xored Software Inc and others.
+ * Copyright (c) 2009, 2020 Xored Software Inc and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -11,7 +11,7 @@
 package org.eclipse.rcptt.internal.launching.ecl;
 
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.rcptt.internal.core.RcpttPlugin;
+import org.eclipse.core.runtime.MultiStatus;
 import org.eclipse.rcptt.internal.launching.ExecutionStatus;
 import org.eclipse.rcptt.internal.launching.Q7LaunchingPlugin;
 import org.eclipse.rcptt.launching.AutLaunch;
@@ -20,18 +20,26 @@ import org.eclipse.rcptt.tesla.ecl.model.GetAdvancedInfo;
 import org.eclipse.rcptt.tesla.ecl.model.TeslaFactory;
 
 public class ExecAdvancedInfoUtil {
-	
+
+	private static AdvancedInformation getAdvancedInfo(AutLaunch launch) throws Exception {
+		AdvancedInformation info = null;
+		final GetAdvancedInfo advInfoCmd = TeslaFactory.eINSTANCE.createGetAdvancedInfo();
+		final Object obj = launch.execute(advInfoCmd);
+		if (obj instanceof AdvancedInformation) {
+			info = (AdvancedInformation) obj;
+		}
+		return info;
+	}
+
 	private static IStatus setAdvancedInfo(AutLaunch launch, ExecutionStatus resultStatus) {
 		AdvancedInformation info = null;
 		try {
 			// try to obtain advanced error information from AUT
-			final GetAdvancedInfo advInfoCmd = TeslaFactory.eINSTANCE
-					.createGetAdvancedInfo();
-			final Object obj = launch.execute(advInfoCmd);
-			if (obj instanceof AdvancedInformation)
-				info = (AdvancedInformation) obj;
+			info = getAdvancedInfo(launch);
 		} catch (Exception e) {
-			return RcpttPlugin.createStatus(e);
+			MultiStatus status = new MultiStatus(Q7LaunchingPlugin.PLUGIN_ID, IStatus.ERROR, e.getMessage(), e);
+			status.add(resultStatus);
+			return status;
 		}
 		if (info != null) {
 			resultStatus.setAdvancedInfo(info);
@@ -40,15 +48,12 @@ public class ExecAdvancedInfoUtil {
 	}
 
 	public static IStatus askForAdvancedInfo(AutLaunch launch, String err) {
-		final ExecutionStatus resultStatus = new ExecutionStatus(IStatus.CANCEL,
-				Q7LaunchingPlugin.PLUGIN_ID, err);
-
+		final ExecutionStatus resultStatus = new ExecutionStatus(IStatus.CANCEL, Q7LaunchingPlugin.PLUGIN_ID, err);
 		return setAdvancedInfo(launch, resultStatus);
 	}
 
 	public static IStatus askForAdvancedInfo(AutLaunch launch, IStatus status) {
 		final ExecutionStatus resultStatus = new ExecutionStatus(status);
-
 		return setAdvancedInfo(launch, resultStatus);
 	}
 

commit 4afcaad5036474fd603adfad806cd486f375c631

Bug 445614: Restore missing cause message in context failure report
This fixes the regression in 86e1122c956190ef23390e8695c3440bd9c12320

launching/org.eclipse.rcptt.launching/src/org/eclipse/rcptt/internal/launching/aut/BaseAutLaunch.java
@@ -295,10 +295,12 @@ public class BaseAutLaunch implements AutLaunch, IBaseAutLaunchRetarget {
 		return (T) result[0];
 	}
 
-	private IStatus createInternalAutFailStatus(IContext context) {
-		final IStatus status = new Status(IStatus.ERROR, CorePlugin.PLUGIN_ID,
+	private IStatus createInternalAutFailStatus(IContext context, IStatus result) {
+		final IStatus status = new MultiStatus(
+				CorePlugin.PLUGIN_ID, 
 				context.isBuiltin() ? IProcess.INTERNAL_AUT_FAILURE : 0,
-				"Failed to apply context " + context.getName(), new FailToExecuteDefaultContext());
+				new IStatus[] {result},
+				"Failed to apply context " + context.getName(), null);
 		final IStatus statusWithAdvancedInfo = ExecAdvancedInfoUtil.askForAdvancedInfo(this, status);
 		return statusWithAdvancedInfo;
 	}
@@ -620,12 +622,12 @@ public class BaseAutLaunch implements AutLaunch, IBaseAutLaunchRetarget {
 		ec.setData(context);
 		try {
 			final IStatus result = internalExecute(ec, TeslaLimits.getContextRunnableTimeout(), monitor, null);
-			IStatus status = createInternalAutFailStatus(contextElement);
+			IStatus status = createInternalAutFailStatus(contextElement,result);
 			if (!result.isOK()) {
 				throw new CoreException(status);
 			}
 		} catch (InterruptedException e) {
-			throw new CoreException(createInternalAutFailStatus(contextElement));
+			throw new CoreException(createInternalAutFailStatus(contextElement, new Status(IStatus.ERROR, CorePlugin.PLUGIN_ID, 0, "Execution interrupted", e)));
 		}
 	}
 