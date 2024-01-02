d5409bf59513dd0362fd738124e5b759ead50d7e
Vasili Gulevich <vasili.gulevich@xored.com>
2024-01-02 20:21:46 +0400

    Fix NPE in Default references removal job

--- rcp/org.eclipse.rcptt.ui/src/org/eclipse/rcptt/internal/ui/ProjectContextReferencesUpdateJob.java
+++ rcp/org.eclipse.rcptt.ui/src/org/eclipse/rcptt/internal/ui/ProjectContextReferencesUpdateJob.java
@@ -14,26 +14,34 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+import org.eclipse.core.resources.IWorkspaceRunnable;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.ui.PlatformUI;
-
-import com.google.common.base.Predicate;
-import com.google.common.collect.Collections2;
 import org.eclipse.rcptt.core.model.IQ7Element.HandleType;
 import org.eclipse.rcptt.core.model.IQ7NamedElement;
 import org.eclipse.rcptt.core.model.IQ7Project;
 import org.eclipse.rcptt.core.model.IQ7ProjectMetadata;
 import org.eclipse.rcptt.core.model.search.ISearchScope;
 import org.eclipse.rcptt.core.model.search.Q7SearchCore;
+import org.eclipse.rcptt.core.scenario.GroupContext;
+import org.eclipse.rcptt.core.scenario.NamedElement;
+import org.eclipse.rcptt.core.scenario.Scenario;
 import org.eclipse.rcptt.internal.core.RcpttPlugin;
 import org.eclipse.rcptt.internal.core.model.OneProjectScope;
 import org.eclipse.rcptt.ui.dialogs.RemoveAllProjectReferencesDialog;
+import org.eclipse.rcptt.ui.utils.WriteAccessChecker;
+import org.eclipse.ui.PlatformUI;
+
+import com.google.common.base.Predicate;
+import com.google.common.collect.Collections2;
 
 public class ProjectContextReferencesUpdateJob extends Job {
 	private IQ7Project project;
@@ -60,6 +68,12 @@ public class ProjectContextReferencesUpdateJob extends Job {
 	@Override
 	protected IStatus run(IProgressMonitor monitor) {
 		try {
+			if (RemoveAllProjectReferencesDialog.isNever()) {
+				return Status.OK_STATUS;
+			}
+			
+			SubMonitor sm = SubMonitor.convert(monitor, 3 );
+				
 			IQ7ProjectMetadata metadata = project.getMetadata();
 			final Set<IQ7NamedElement> elementsToProceed = new HashSet<IQ7NamedElement>();
 			ISearchScope scope = new OneProjectScope(project);
@@ -67,7 +81,7 @@ public class ProjectContextReferencesUpdateJob extends Job {
 			String[] contexts = metadata.exists()?metadata.getContexts():new String[0];
 			for (String el : contexts) {
 				IQ7NamedElement[] elements = Q7SearchCore.findContextUsage(el,
-						scope, new NullProgressMonitor());
+						scope, sm.split(1));
 				if (elements != null) {
 					elementsToProceed.addAll(filterOutProjectMetadata(elements));
 				}
@@ -77,30 +91,97 @@ public class ProjectContextReferencesUpdateJob extends Job {
 			String[] verifications = metadata.getVerifications();
 			for (String el : verifications) {
 				IQ7NamedElement[] elements = Q7SearchCore.findVerificationUsage(el,
-						scope, new NullProgressMonitor());
+						scope,sm.split(1));
 				if (elements != null) {
 					elementsToProceed.addAll(filterOutProjectMetadata(elements));
 				}
 
 			}
-
+			
+			boolean[] remove = { false }; 
+			ArrayList<IQ7NamedElement> copy = new ArrayList<IQ7NamedElement>(
+					elementsToProceed);
 			if (elementsToProceed.size() > 0) {
 				PlatformUI.getWorkbench().getDisplay()
-						.asyncExec(new Runnable() {
+						.syncExec(new Runnable() {
 							public void run() {
-								RemoveAllProjectReferencesDialog.open(
+								remove[0] = RemoveAllProjectReferencesDialog.open(
 										PlatformUI.getWorkbench()
 												.getWorkbenchWindows()[0]
-												.getShell(), project,
-										new ArrayList<IQ7NamedElement>(
-												elementsToProceed));
+												.getShell(), project,copy
+										);
+								}
 							}
-						});
+						);
 			}
+			if (remove[0]) {
+				return removeDefaultReferences(project, copy, sm.split(1));
+			}				
 		} catch (Exception e) {
 			RcpttPlugin.log(e);
 		}
 		return Status.OK_STATUS;
+	}
+
+	private static IStatus removeDefaultReferences(final IQ7Project project,
+			final List<IQ7NamedElement> references, IProgressMonitor monitorArg) {
+		final IStatus[] status = { Status.OK_STATUS };
+		try {
+			IQ7ProjectMetadata metadata = project.getMetadata();
+			if (metadata == null || !metadata.exists()) {
+				return status[0];
+			}
+			String[] defaultContexts = metadata.getContexts();
+			String[] defaultVerifications = metadata.getVerifications();
+			if (defaultContexts.length == 0 && defaultVerifications.length == 0) {
+				return status[0];
+			}
+			final List<String> contextsToRemove = Arrays.asList(defaultContexts);
+			final List<String> verificationsToRemove = Arrays.asList(defaultVerifications);
+			ResourcesPlugin.getWorkspace().run(
+					new IWorkspaceRunnable() {
+						public void run(IProgressMonitor monitor)
+								throws CoreException {
+							SubMonitor sm = SubMonitor.convert(monitor,
+									"Remove project context/verification references", references.size() * 2);
+							WriteAccessChecker writeAccessChecker = new WriteAccessChecker(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell());
+							if (!writeAccessChecker.makeResourceWritable(references
+									.toArray(new IQ7NamedElement[0]))) {
+								status[0] = Status.CANCEL_STATUS;
+								return;
+							}
+							for (IQ7NamedElement e : references) {
+								IQ7NamedElement copy = e.getWorkingCopy(sm.split(1));
+								try {
+									NamedElement ne = copy.getNamedElement();
+									if (ne instanceof Scenario) {
+										((Scenario) ne)
+												.getContexts()
+												.removeAll(
+														contextsToRemove);
+										((Scenario) ne)
+												.getVerifications()
+												.removeAll(
+														verificationsToRemove);
+									}
+									if (ne instanceof GroupContext) {
+										((GroupContext) ne)
+												.getContextReferences()
+												.removeAll(
+														contextsToRemove);
+									}
+									copy.commitWorkingCopy(false,
+											sm.split(1));
+								} finally {
+									copy.discardWorkingCopy();
+								}
+							}
+						}
+					}, monitorArg);
+		} catch (CoreException e) {
+			return e.getStatus();
+		}
+		return status[0];
 	}
 
 	@Override
--- rcp/org.eclipse.rcptt.ui/src/org/eclipse/rcptt/ui/dialogs/RemoveAllProjectReferencesDialog.java
+++ rcp/org.eclipse.rcptt.ui/src/org/eclipse/rcptt/ui/dialogs/RemoveAllProjectReferencesDialog.java
@@ -10,88 +10,47 @@
  *******************************************************************************/
 package org.eclipse.rcptt.ui.dialogs;
 
-import java.util.Arrays;
 import java.util.List;
 
-import org.eclipse.core.resources.IWorkspaceRunnable;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.SubProgressMonitor;
-import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.dialogs.IDialogConstants;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.dialogs.MessageDialogWithToggle;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.rcptt.core.model.IQ7NamedElement;
 import org.eclipse.rcptt.core.model.IQ7Project;
-import org.eclipse.rcptt.core.model.IQ7ProjectMetadata;
-import org.eclipse.rcptt.core.scenario.GroupContext;
-import org.eclipse.rcptt.core.scenario.NamedElement;
-import org.eclipse.rcptt.core.scenario.Scenario;
 import org.eclipse.rcptt.internal.ui.Q7UIPlugin;
 import org.eclipse.rcptt.ui.preferences.IPreferenceKeys;
-import org.eclipse.rcptt.ui.utils.WriteAccessChecker;
-import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Shell;
 
-public class RemoveAllProjectReferencesDialog extends MessageDialogWithToggle {
+public class RemoveAllProjectReferencesDialog {
 
 	private static IPreferenceStore prefStore = Q7UIPlugin.getDefault()
 			.getPreferenceStore();
-
-	private final List<IQ7NamedElement> references;
-	private final IQ7Project project;
-
-	private static RemoveAllProjectReferencesDialog instance;
 
 	public static boolean open(Shell parentShell, IQ7Project project,
 			List<IQ7NamedElement> elements) {
 		String value = prefStore
 				.getString(IPreferenceKeys.ALL_REMOVE_PROJECT_REFERENCES);
-		if (ALWAYS.equals(value)) {
-			addMissingReferences(project, elements);
+		if (MessageDialogWithToggle.ALWAYS.equals(value)) {
 			return true;
+		}
+		if (MessageDialogWithToggle.NEVER.equals(value)) {
+			return false;
 		} else {
-			if (instance != null && instance.getShell() != null
-					&& !instance.getShell().isDisposed()) {
-				// Is still running
-				return false;
-			}
-			instance = new RemoveAllProjectReferencesDialog(parentShell,
-					project, elements);
-			return instance.open() == IDialogConstants.YES_ID;
+			MessageDialogWithToggle dialog = MessageDialogWithToggle.openYesNoQuestion(parentShell,
+					"Project Context and Verification References", generateMessage(elements),
+					null, false, prefStore, value);
+			return dialog.open() == IDialogConstants.YES_ID;
 		}
 	}
 
-	private RemoveAllProjectReferencesDialog(Shell parentShell,
-			IQ7Project project, List<IQ7NamedElement> references) {
-		super(
-				parentShell,
-				"Project Context and Verification References",
-				null,
-				null,
-				MessageDialog.NONE,
-				new String[] { IDialogConstants.YES_LABEL,
-						IDialogConstants.NO_LABEL },
-				0,
-				"Always remove default project context and verification references automatically.",
-				false);
-		this.project = project;
-		this.references = references;
-		this.message = generateMessage(references);
-		setShellStyle(SWT.SHEET);
-	}
-
-	private String generateMessage(List<IQ7NamedElement> references) {
+	private static String generateMessage(List<IQ7NamedElement> references) {
 		StringBuilder label = new StringBuilder(
 				"Would you like to remove default project context/verification references from "
 						+ references.size() + " elements?\n\n");
 		// try {
 		// for (IQ7NamedElement e : references) {
-		//				label.append("    ").append(e.getNamedElement().getName()).append(" (").append(e.getPath()).append(")").append("\n"); //$NON-NLS-1$ //$NON-NLS-2$
+		// label.append(" ").append(e.getNamedElement().getName()).append("
+		// (").append(e.getPath()).append(")").append("\n"); //$NON-NLS-1$ //$NON-NLS-2$
 		// }
 		// } catch (ModelException e) {
 		// Q7UIPlugin.log(e);
@@ -99,86 +58,10 @@ public class RemoveAllProjectReferencesDialog extends MessageDialogWithToggle {
 		return label.toString();
 	}
 
-	@Override
-	protected void buttonPressed(int buttonId) {
-		if (buttonId == IDialogConstants.YES_ID) {
-			addMissingReferences(project, references);
-		}
-		if (getToggleState()) {
-			prefStore.setValue(IPreferenceKeys.ALL_REMOVE_PROJECT_REFERENCES,
-					ALWAYS);
-		}
-		setReturnCode(buttonId);
-		close();
-	}
 
-	private static void addMissingReferences(final IQ7Project project,
-			final List<IQ7NamedElement> references) {
-		Job job = new Job("Fix project references") {
-			@Override
-			protected IStatus run(IProgressMonitor monitor) {
-				final IStatus[] status = { Status.OK_STATUS };
-				try {
-					monitor.beginTask("Remove project context/verification references",
-							references.size() * 2);
-					IQ7ProjectMetadata metadata = project.getMetadata();
-					if (metadata == null || !metadata.exists()) {
-						return status[0];
-					}
-					String[] defaultContexts = metadata.getContexts();
-					String[] defaultVerifications = metadata.getVerifications();
-					if (defaultContexts.length == 0 && defaultVerifications.length == 0) {
-						return status[0];
-					}
-					final List<String> contextsToRemove = Arrays.asList(defaultContexts);
-					final List<String> verificationsToRemove = Arrays.asList(defaultVerifications);
-					ResourcesPlugin.getWorkspace().run(
-							new IWorkspaceRunnable() {
-								public void run(IProgressMonitor monitor)
-										throws CoreException {
-									WriteAccessChecker writeAccessChecker = new WriteAccessChecker(instance.getShell());
-									if (!writeAccessChecker.makeResourceWritable(references
-											.toArray(new IQ7NamedElement[0]))) {
-										status[0] = Status.CANCEL_STATUS;
-										return;
-									}
-									for (IQ7NamedElement e : references) {
-										IQ7NamedElement copy = e.getWorkingCopy(new SubProgressMonitor(
-												monitor, 1));
-										try {
-											NamedElement ne = copy.getNamedElement();
-											if (ne instanceof Scenario) {
-												((Scenario) ne)
-														.getContexts()
-														.removeAll(
-																contextsToRemove);
-												((Scenario) ne)
-														.getVerifications()
-														.removeAll(
-																verificationsToRemove);
-											}
-											if (ne instanceof GroupContext) {
-												((GroupContext) ne)
-														.getContextReferences()
-														.removeAll(
-																contextsToRemove);
-											}
-											copy.commitWorkingCopy(false,
-													new SubProgressMonitor(
-															monitor, 1));
-										} finally {
-											copy.discardWorkingCopy();
-										}
-									}
-								}
-							}, monitor);
-				} catch (CoreException e) {
-					return e.getStatus();
-				}
-				return status[0];
-			}
-		};
-		job.setUser(true);
-		job.schedule();
+	public static boolean isNever() {
+		String value = prefStore
+				.getString(IPreferenceKeys.ALL_REMOVE_PROJECT_REFERENCES);
+		return MessageDialogWithToggle.NEVER.equals(value);
 	}
 }
