c11291a6ba92d71ee89f7eeaec140f8ca3ec6685
Vasili Gulevich <vasili.gulevich@xored.com>
2024-04-14 21:28:31 +0400

    Fix "The constructor is undefined"

--- runtime/tests/org.eclipse.rcptt.tesla.swt.test/src/org/eclipse/rcptt/tesla/swt/test/NoErrorsInLog.java
+++ runtime/tests/org.eclipse.rcptt.tesla.swt.test/src/org/eclipse/rcptt/tesla/swt/test/NoErrorsInLog.java
@@ -35,7 +35,7 @@ public class NoErrorsInLog extends ExternalResource {
 	
 	public NoErrorsInLog(Class<?> clazzToWatchLogFor) {
 		this.log = Platform.getLog(FrameworkUtil.getBundle(clazzToWatchLogFor));
-		this.statusLog = new MultiStatus(clazzToWatchLogFor, 0, "Errors in the error log");
+		this.statusLog = new MultiStatus("org.eclipse.rcptt.tesla.swt.test", 0, "Errors in the error log", null);
 	}
 	@Override
 	protected void before() throws Throwable {
